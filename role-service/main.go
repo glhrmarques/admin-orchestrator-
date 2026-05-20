d6d0ea5a63ce62a2b6f63b2396e1cbc34bc381b2
glhrmarques <gmarques.pd@gmail.com>
2026-05-19 21:20:19 -0300

    roles

--- role-service/main.go
+++ role-service/main.go
@@ -12,7 +12,7 @@ type user struct {
 }
 
 var users = []user {
-	{Email:"admin@admin.com", Password:"admin@123", Role:"admin"},
+	{Email:"admin@admin.com", Password:"123", Role:"admin"},
 	{Email:"operator@operator.com", Password:"operator@123", Role:"operator"},
 }
 
@@ -21,8 +21,20 @@ var users = []user {
 func main() {
 	router := gin.Default()
 
+	router.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	})
+
 	router.GET("/getUsers", getAllUsers)
 	router.POST("/addUser", createUser)
+	router.POST("/login", loginUser)
 	router.Run("localhost:8080")
 }
 
@@ -42,4 +54,25 @@ func createUser(c *gin.Context) {
 	users = append(users, newUser)
 	c.IndentedJSON(http.StatusCreated, newUser)
 
+}
+
+func loginUser (c *gin.Context){
+	var credentials struct {
+		Email string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BindJSON(&credentials); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	for _, u := range users {
+		if u.Email == credentials.Email && u.Password == credentials.Password {
+			c.JSON(http.StatusOK, gin.H{"role": u.Role})
+			return
+		}
+	}
+
+	c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or passoword"})
 }
